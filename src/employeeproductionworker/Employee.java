f63b2a16f183d1f685c4a5df1f027a0626b2a350
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-04-22 17:55:06 -0400

    Initial commit.

---
+++ src/employeeproductionworker/Employee.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeeproductionworker;
+
+/**
+ *
+ * @author bluebackdev
+ */
+public class Employee {
+    private String name;
+    private int
+}
